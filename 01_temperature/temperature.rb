7aecf0ea0fc3f9e472736a456d3c1efde1499346
Alex Sutton <alexsutton14@hotmail.com>
2019-08-25 16:25:37 +0100

    Completed 01 Temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+def ftoc(inputF)
+    inputFFloat = inputF.to_f
+    (inputFFloat - 32)*(5.0/9.0)
+end
+def ctof(inputC)
+    inputCFloat = inputC.to_f
+    (inputCFloat * (9.0/5.0))+32
+end
